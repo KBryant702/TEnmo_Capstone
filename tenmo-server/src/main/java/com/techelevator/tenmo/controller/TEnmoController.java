afc13f043f7c690bdd58004c5292cdba0572a951
Kim <99856852+KBryant702@users.noreply.github.com>
2022-06-17 13:11:49 -0700

    @RequestMapping added

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TEnmoController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TEnmoController.java
@@ -5,6 +5,7 @@ import com.techelevator.tenmo.dao.*;
 import com.techelevator.tenmo.exceptions.InsufficientFunds;
 import com.techelevator.tenmo.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,61 +14,61 @@ import java.security.Principal;
 import java.util.List;
 
 @RestController
-//@RequestMapping(path =) //// NEEDS PATH
 @PreAuthorize("isAuthenticated()")
 public class TEnmoController {
-    
+
     @Autowired
     AccountDao accountDao;
-    
+
     @Autowired
     UserDao userDao;
-    
+
     @Autowired
     TransferDao transferDao;
-    
+
     @Autowired
     TransferTypeDao transferTypeDao;
-    
+
     @Autowired
     TransferStatusDao transferStatusDao;
-    
+
     @GetMapping(path = "/balance")
-    public Balance getBalance(Principal principal){
+    public Balance getBalance(Principal principal) {
         System.out.println(principal.getName());
         return accountDao.getBalance(principal.getName());
     }
-    
+
     @GetMapping(path = "/tenmo_user")
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userDao.findAll();
     }
 
-    @GetMapping(path= "/transfer")
+    @GetMapping(path = "/transfer")
     public List<Transfer> getAllTransfers() {
         return transferDao.getAllTransfers();
     }
 
-    @GetMapping(path="/transferType/filter")
+    @GetMapping(path = "/transferType/filter")
     public TransferType getTransferTypeByTypeDesc(@RequestParam String desc) {
         return transferTypeDao.getTransferTypeByTypeDesc(desc);
     }
-    
+
     @GetMapping(path = "/transferStatus/filter")
-    public TransferStatus getTransferStatusByDesc(@RequestParam String desc){
+    public TransferStatus getTransferStatusByDesc(@RequestParam String desc) {
         return transferStatusDao.getByStatusDesc(desc);
     }
 
-    @GetMapping(path="/transferstatus/{id}")
+    @GetMapping(path = "/transferstatus/{id}")
     public TransferStatus getTransferStatusFromId(@PathVariable long transferStatusId) {
         return transferStatusDao.getByStatusId(transferStatusId);
     }
 
-    @GetMapping(path="/transfers/tenmo_user/{userId}/pending")
+    @GetMapping(path = "/transfers/tenmo_user/{userId}/pending")
     public List<Transfer> getPendingTransfersByUserId(@PathVariable long userId) {
         return transferDao.getPendingTransfers(userId);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "/transfer/{id}")
     public void createTransfer(@RequestBody Transfer transfer, @PathVariable long id) throws InsufficientFunds {
         BigDecimal transferAmount = transfer.getAmount();
@@ -81,9 +82,9 @@ public class TEnmoController {
         accountDao.updateAccount(accountTo);
     }
 
-    @GetMapping(path="/transfers")
+    @GetMapping(path = "/transfers")
     public void updateTransferStatus(@RequestBody Transfer transfer) throws InsufficientFunds {
-        if(transfer.getTransferStatusId() == transferStatusDao.getByStatusDesc("Approved").getTransferStatusId()) {
+        if (transfer.getTransferStatusId() == transferStatusDao.getByStatusDesc("Approved").getTransferStatusId()) {
             BigDecimal amountToTransfer = transfer.getAmount();
             Account accountFrom = accountDao.findAccountByAccountId(transfer.getAccountFrom());
             Account accountTo = accountDao.findAccountByAccountId(transfer.getAccountTo());
@@ -97,24 +98,24 @@ public class TEnmoController {
             transferDao.updateTransfer(transfer);
         }
     }
-    
+
     @GetMapping(path = "/account/{userId}")
-    public Account findAccountByUserId(@PathVariable long userId){
+    public Account findAccountByUserId(@PathVariable long userId) {
         return accountDao.findAccountByUserId(userId);
     }
-    
+
     @GetMapping(path = "/account/{accountId}")
-    public Account findAccountByAccountId(@PathVariable long accountId){
+    public Account findAccountByAccountId(@PathVariable long accountId) {
         return accountDao.findAccountByAccountId(accountId);
     }
-    
+
     @GetMapping(path = "/transfer/tenmo_user/{userId}")
-    public List<Transfer> getTransfersByUserId(@PathVariable long userId){
+    public List<Transfer> getTransfersByUserId(@PathVariable long userId) {
         return transferDao.getTransfersByUserId(userId);
     }
-    
+
     @GetMapping(path = "/tenmo_user/{userId}")
-    public User getUserByUserId(@PathVariable long userId){
+    public User getUserByUserId(@PathVariable long userId) {
         return userDao.getUserByUserId(userId);
     }
 }
